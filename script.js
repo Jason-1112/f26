88a82d588f1dfb192bc53a3e94ad1e9c85993ab8
name <email>
2025-02-26 12:26:28 +1100

    commit 2

---
+++ script.js
@@ -0,0 +1,66 @@
+// console.log('Hello, world!');
+// console.log("hi jalal");
+
+// let name = "jalal";
+// let address = "Sydney";
+// let age = 50;
+
+// console.log(name)
+// console.log(address)
+// console.log(age)
+
+// //if statement:   if (condition = ?) {do this}
+// if (age === 10) //always better to use triple = for comparison
+//     {
+//     console.log ("this is a minor")
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) { console.log("fizz buzz") }
+//     if (i % 3 === 0) { console.log("fizz"); }
+//     else if (i % 5 === 0) { console.log("buzz"); }
+//     else { console.log(i) }
+// };
+
+// let score = 10;
+
+// if (score < 0 || score > 100) {
+//     console.log ("invalid score")
+// };
+// else if (score >= 90) {
+//     console.log ("score: A")
+// };
+// else if (score >= 80) {
+//     console.log ("score: B")
+// };
+// else if (score >= 70) {
+//     console.log ("score: C")
+// };
+// else if (score >= 60) {
+//     console.log ("score: D")
+// };
+// else if (score <= 59 ){
+//     console.log ("score: F")
+// };
+
+
+// const numList = [1,2,3,4,5,6];
+
+// let total = 0;
+//     for (let i = 0;i < numList.length; i++)
+//     {
+//         total = total + numList [i]
+//     }
+
+// console.log(total)
+
+let a = [12,34,56,1]
+
+max = 0
+
+for (let i = 0;i < a.length; i++)
+{
+    if (max < a[i])
+        max = a[i]
+}
+console.log(max)
